f8e87f0eb9ae936a2ac1f8c2aeee5a5a5e0c22a9
Lewis Trowbridge <lewbob9927@gmail.com>
2021-12-20 20:39:36 +0000

    Added call to Github API

---
+++ Gitignorerer.Tests/API/GithubGitignoreClientTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using Gitignorerer.API;
+using Xunit;
+using Moq;
+using Moq.Contrib.HttpClient;
+using FluentAssertions;
+
+namespace Gitignorerer.Tests.API
+{
+    public class GithubGitignoreClientTest
+    {
+        private readonly GithubGitignoreClient _githubGitignoreClient;
+        private readonly Mock<HttpMessageHandler> _mockHttpMessageHandler;
+        private readonly HttpClient _mockHttpClient;
+        
+        public GithubGitignoreClientTest()
+        {
+            _mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            _mockHttpClient = _mockHttpMessageHandler.CreateClient();
+
+            _githubGitignoreClient = new GithubGitignoreClient(_mockHttpClient);
+        }
+
+        [Fact]
+        public async void GithubGitignoreClient_WhenCalled_CallsCorrectUrl()
+        {
+            var expectedUrl = "https://api.github.com/gitignore/templates";
+            _mockHttpMessageHandler.SetupRequest(expectedUrl).ReturnsResponse(HttpStatusCode.OK, "");
+
+            await _githubGitignoreClient.GetTemplateNames();
+
+            _mockHttpMessageHandler.VerifyRequest(expectedUrl);
+        }
+    }
+}
---
+++ Gitignorerer/API/GithubGitignoreClient.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace Gitignorerer.API
+{
+    public class GithubGitignoreClient
+    {
+        private readonly HttpClient _client;
+
+        public GithubGitignoreClient(HttpClient httpClient)
+        {
+            _client = httpClient;
+            httpClient.BaseAddress = new Uri("https://api.github.com");
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3.raw"));
+        }
+
+        public async Task<string[]> GetTemplateNames()
+        {
+            var response = await _client.GetAsync("/gitignore/templates");
+            var templateString = await response.Content.ReadAsStringAsync();
+            return SplitTemplateListIntoStringArray(templateString);
+        }
+
+        private string[] SplitTemplateListIntoStringArray(string templateListString)
+        {
+            return templateListString
+                .Replace("[", "")
+                .Replace("]", "")
+                .Split(Environment.NewLine);
+        }
+    }
+}

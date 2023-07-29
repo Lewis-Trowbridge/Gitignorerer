80208323ed83c0a395fa956096a2233e0d7d01f3
Lewis Trowbridge <lewbob9927@gmail.com>
2023-07-29 17:55:16 +0100

    Used simplified top level statements

--- Gitignorerer/Program.cs
+++ Gitignorerer/Program.cs
@@ -1,43 +1,35 @@
-﻿using Gitignorerer.API;
+﻿using Gitignorerer;
+using Gitignorerer.API;
 using Gitignorerer.IO;
 using Gitignorerer.Parsers;
 using Gitignorerer.Utils;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Gitignorerer
+var services = new ServiceCollection()
+    .AddSingleton<IGitignorererApplication, GitignorererApplication>()
+    .AddSingleton(PhysicalConsole.Singleton)
+    .AddSingleton<IConsoleWrapper, ConsoleWrapper>()
+    .AddHttpClient()
+    .AddSingleton<IGitignoreClient, GithubGitignoreClient>()
+    .AddSingleton<IGitignoreWriter, GitignoreWriter>()
+    .BuildServiceProvider();
+
+var app = new CommandLineApplication() 
+{ 
+    Name = "gitignorerer",
+    Description = "A tool to make Gitignore files easily.",
+};
+app.ValueParsers.Add(new StringToHashSetParser());
+app.HelpOption();
+var ignoreFiles = app.Argument<HashSet<string>>("Ignore files", "Ignore file names to add to a .gitignore file", multipleValues: true).IsRequired();
+app.Conventions
+    .UseDefaultConventions()
+    .UseConstructorInjection(services);
+
+app.OnExecuteAsync(async cancellationtoken =>
 {
-    public class Program
-    {
-        public static Task<int> Main(string[] args)
-        {
-            var services = new ServiceCollection()
-                .AddSingleton<IGitignorererApplication, GitignorererApplication>()
-                .AddSingleton(PhysicalConsole.Singleton)
-                .AddSingleton<IConsoleWrapper, ConsoleWrapper>()
-                .AddHttpClient()
-                .AddSingleton<IGitignoreClient, GithubGitignoreClient>()
-                .AddSingleton<IGitignoreWriter, GitignoreWriter>()
-                .BuildServiceProvider();
+    await services.GetRequiredService<IGitignorererApplication>().Run(ignoreFiles.ParsedValue);
+});
 
-            var app = new CommandLineApplication() 
-            { 
-                Name = "gitignorerer",
-                Description = "A tool to make Gitignore files easily.",
-            };
-            app.ValueParsers.Add(new StringToHashSetParser());
-            app.HelpOption();
-            var ignoreFiles = app.Argument<HashSet<string>>("Ignore files", "Ignore file names to add to a .gitignore file", multipleValues: true).IsRequired();
-            app.Conventions
-                .UseDefaultConventions()
-                .UseConstructorInjection(services);
-
-            app.OnExecuteAsync(async cancellationtoken =>
-            {
-                await services.GetRequiredService<IGitignorererApplication>().Run(ignoreFiles.ParsedValue);
-            });
-
-            return app.ExecuteAsync(args);
-        }
-    }
-}
+return await app.ExecuteAsync(args);
